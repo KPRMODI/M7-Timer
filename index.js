c043109cdba7bf45df7366372514a2360a1aad98
KPRMODI <kpmodi@my.waketech.edu>
2025-03-14 13:19:33 -0400

    Added comments

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 let timer = document.createElement("p")
-const timercounter = document.getElementById("timercounter")
+const timercounter = document.getElementById("timercounter") // timer counter
 
 timer.value = 0
-timer.textContent = 0
+timer.textContent = 0 // values for timer
 
 timercounter.appendChild(timer)
 
 let startbutton = document.getElementById("startTimer")
-let stopbutton = document.getElementById("stopTimer")
+let stopbutton = document.getElementById("stopTimer") // all buttons
 let resetbutton = document.getElementById("resetTimer")
 
-let timerifon = true
+let timerifon = true // determines if start is clicked or not
 
 startbutton.addEventListener("click", start)
-stopbutton.addEventListener("click", end)
+stopbutton.addEventListener("click", end) // event listeners
 resetbutton.addEventListener("click", reset)
 
 function start(){
-    if (timerifon == true){
+    if (timerifon == true){ // if start, interval every second
         interval = setInterval(timerfr, 1000)
     }
-    timerifon = false
+    timerifon = false // makes it so start cant be clicked again
 }
 
 function timerfr() {
-    timercounter.innerHTML = timer.value += 1
+    timercounter.innerHTML = timer.value += 1 // adds one
 }
 
 function end(){
-    clearInterval(interval)
-    console.log("hi")
+    clearInterval(interval) // clears interval
     timerifon = true
 }
 
 function reset(){
     clearInterval(interval)
-    timer.value = 0
+    timer.value = 0 // resets timer
     timercounter.innerHTML = timer.value
     timerifon = true
 }
